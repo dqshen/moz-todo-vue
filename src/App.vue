03f65da81f84aa5f9ef4bb798971cecd48f403d2
dqs <dqs@dqs.com>
2021-12-24 15:32:53 +0800

    添加v-for语法示例

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,36 @@
 <template>
   <div id="app">
-  <h1>To-Do List</h1>
-  <ul>
-    <li>
-      <!-- label是给prop里的label赋值 -->
-      <!-- :done是v-bind:done的缩写，告知vue cli这是给todoitem的prop对象done按照它的类型(boolean)赋值 -->
-     <to-do-item label="My Todo Item" :done="false"></to-do-item>
-    </li>
-  </ul>
-</div>
-
+    <h1>To-Do List</h1>
+    <ul>
+      <!-- 用 v-for ... in ... 语法创建item枚举后,li下的属性都可以用item以及它的子属性赋值了,赋值时需要用v-bind语法转义-->
+      <li v-for="item in ToDoItems" :key="item.id">
+        <!-- label是给prop里的label赋值 -->
+        <!-- :done是v-bind:done的缩写，告知vue cli这是给todoitem的prop对象done按照它的类型(boolean)赋值 -->
+        <to-do-item :label="item.label" :done="item.done"></to-do-item>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 import ToDoItem from "./components/ToDoItem.vue";
+import uniqueid from 'lodash.uniqueid'
+
 export default {
   name: "App",
   components: {
     ToDoItem,
   },
+  data() {
+    return {
+      ToDoItems: [
+        { id: uniqueid('todo-'), label: "Learn Vue", done: false },
+        { id: uniqueid('todo-'), label: "Create a Vue project with the CLI", done: true },
+        { id: uniqueid('todo-'), label: "Have fun", done: true },
+        { id: uniqueid('todo-'), label: "Create a to-do list", done: false },
+      ]
+    };
+  }
 };
 </script>
 
--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <!-- 使用前缀v-bind:或者简写前缀:将标签的attribute与一个data()里的绑定关联 -->
-    <input type="checkbox" id="todo-item" v-bind:checked="isDone"/>
+    <input type="checkbox" :id="id" v-bind:checked="isDone" />
     <!-- 使用{{ }}语法可以在template标签的内容位置调用注册在props里的字段 -->
-    <label for="todo-item">{{label}}</label>
+    <label :for="id">{{ label }}</label>
   </div>
 </template>
 <script>
-export default {
+import uniqueid from "lodash.uniqueid";
+
+export default {  
   // props是专门用于存放属性值prop的标签，这种声明的特点是可以给每个prop增加约束条件
   //    type: 告知vue cli这个prop的类型，方便检查语法错误，仅在开发阶段有效
   //    required: 约定这个prop在组件被调用时需要被定义
@@ -22,7 +24,8 @@ export default {
       // 定义一个名为isDone的绑定，绑定的作用是把模版里标签的属性值与脚本里props中的属性值绑定到一起
       // isDone这个绑定负责关联注册在props里面的done属性
       // template中使用v-bind:调用这个绑定，把绑定标记的this.done和被绑定的template标签属性checked关联起来，完成绑定
-      isDone: this.done
+      isDone: this.done,
+      id: uniqueid('todo-')
     };
   }
 };
