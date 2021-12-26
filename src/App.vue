56e8c4940e322c73f1788cac9694a5f3ef94335a
dqshen <dqshen@hogwarts>
2021-12-26 16:00:34 +0800

    在主页面上用v-on接收ToDoForm的自定义事件

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div id="app">
     <h1>To-Do List</h1>
     <ul>
+      <!-- v-on语法可以为自定义事件绑定回调 -->
+      <!-- 这里绑定的自定义事件是todo-added，等价于ele.addEventListenr('todo-added',...)语句 -->
+      <!-- 回调函数可以从methods里获取 -->
+      <to-do-form @todo-added="addToDo"></to-do-form>
       <!-- 用 v-for ... in ... 语法创建item枚举后,li下的属性都可以用item以及它的子属性赋值了,赋值时需要用v-bind语法转义-->
       <li v-for="item in ToDoItems" :key="item.id">
         <!-- label是给prop里的label赋值 -->
@@ -14,23 +18,34 @@
 
 <script>
 import ToDoItem from "./components/ToDoItem.vue";
-import uniqueid from 'lodash.uniqueid'
+import ToDoForm from "./components/ToDoForm.vue";
+import uniqueid from "lodash.uniqueid";
 
 export default {
   name: "App",
   components: {
     ToDoItem,
+    ToDoForm,
   },
   data() {
     return {
       ToDoItems: [
-        { id: uniqueid('todo-'), label: "Learn Vue", done: false },
-        { id: uniqueid('todo-'), label: "Create a Vue project with the CLI", done: true },
-        { id: uniqueid('todo-'), label: "Have fun", done: true },
-        { id: uniqueid('todo-'), label: "Create a to-do list", done: false },
-      ]
+        { id: uniqueid("todo-"), label: "Learn Vue", done: false },
+        {
+          id: uniqueid("todo-"),
+          label: "Create a Vue project with the CLI",
+          done: true,
+        },
+        { id: uniqueid("todo-"), label: "Have fun", done: true },
+        { id: uniqueid("todo-"), label: "Create a to-do list", done: false },
+      ],
     };
-  }
+  },
+  methods: {
+    addToDo() {
+      console.log("To-do added");
+    },
+  },
 };
 </script>
 
