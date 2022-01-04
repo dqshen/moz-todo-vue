0ac572f094316deb79b9c77ad7653c638fa2e731
dqshen <dqshen@hogwarts>
2022-01-04 23:19:36 +0800

    给ToDoItem添加编辑元素

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,19 +1,29 @@
 <template>
-  <div class="custom-checkbox">
-    <!-- html中的input是一个labelable标签（一个表单元素），它的id值会将它与上下文中用for标记了相同id值的label绑定-->
-    <!-- 使用前缀v-bind:或者简写前缀:将标签的attribute与一个data()里的对象关联 -->
-    <!-- v-on语法使用in-line方式发出一个事件，没有去绑定methods()中的方法 -->
-    <input
-      type="checkbox"
-      class="checkbox"
-      :id="id"
-      v-bind:checked="isDone"
-      @change="$emit('checkbox-changed')"
-    />
-    <!-- 使用{{ }}语法可以在template标签的内容位置调用注册在props里的字段 -->
-    <!-- label的for属性用来标记它的id值，这个id值将它与第一个有相同id值的labelable对象绑定 -->
-    <!-- 可以有多个label有相同的for值，它们会绑定到同一个labelable标签上 -->
-    <label class="checkbox-label" :for="id">{{ label }}</label>
+  <div class="stack-small">
+    <div class="custom-checkbox">
+      <!-- html中的input是一个labelable标签（一个表单元素），它的id值会将它与上下文中用for标记了相同id值的label绑定-->
+      <!-- 使用前缀v-bind:或者简写前缀:将标签的attribute与一个data()里的对象关联 -->
+      <!-- v-on语法使用in-line方式发出一个事件，没有去绑定methods()中的方法 -->
+      <input
+        type="checkbox"
+        class="checkbox"
+        :id="id"
+        v-bind:checked="isDone"
+        @change="$emit('checkbox-changed')"
+      />
+      <!-- 使用{{ }}语法可以在template标签的内容位置调用注册在props里的字段 -->
+      <!-- label的for属性用来标记它的id值，这个id值将它与第一个有相同id值的labelable对象绑定 -->
+      <!-- 可以有多个label有相同的for值，它们会绑定到同一个labelable标签上 -->
+      <label class="checkbox-label" :for="id">{{ label }}</label>
+    </div>
+    <div class="btn-group">
+      <button type="button" class="btn" @click="toggleToItemEditForm">
+        Edit <span class="visually-hidden">{{ label }}</span>
+      </button>
+      <button type="button" class="btn btn__danger" @click="deleteToDo">
+        Delete <span class="visually-hidden">{{ label }}</span>
+      </button>
+    </div>
   </div>
 </template>
 <script>
@@ -26,7 +36,7 @@ export default {
   props: {
     label: { required: true, type: String },
     done: { default: false, type: Boolean },
-    id: { required: true, type: String }
+    id: { required: true, type: String },
   },
   // data是专门用于声明component内模型对象的方法
   // data里声明的对象是跟着component一起的，当component被引用/被创建时，每个component实例都有自己的一组对象，类似于类的对象
@@ -35,7 +45,16 @@ export default {
       // 定义一个名为isDone的对象，并且用props里的done进行赋值，类似于类的构造函数在创建类的实例时，给类的对象赋值
       // template中需要使用v-bind:语法调用这个对象，调用时这个对象已经通过框架赋值完毕
       isDone: this.done,
+      isEditing: false,
     };
+  },
+  methods: {
+    deleteToDo() {
+      this.$emit("item-deleted");
+    },
+    toggleToItemEditForm() {
+      this.isEditing = true;
+    },
   },
 };
 </script>
