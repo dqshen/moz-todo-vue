306f03f65a7b4ac075371e9ba38b45d69ffb2b45
dqshen <dqshen@hogwarts>
2022-02-14 15:46:04 +0800

    为npm run build命令增加资源路径配置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     configureWebpack: {
       devtool: "source-map"
-    }
+    },
+    publicPath: './', // webpack打包时, 所有的js和css插入到index.html的时候, 使用相对路径，也就是以此时的html所在的目录为根目录(dist下)去找js和css文件
+
   };
